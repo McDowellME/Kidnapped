d92a5a022af2c56d3d8cdd01c7a6b2636cd9b54f
Daniel Harris <dan_harris10@yahoo.com>
2022-04-30 16:43:45 -0700

    refactored code

---
+++ src/com/sprints/OurJSONParser.java
@@ -0,0 +1,99 @@
+package com.sprints;
+
+import com.sprints.controller.App;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class OurJSONParser {
+
+    private static OurJSONParser ourParser = null;
+
+    private static final String COMMANDS = "/commands.json";
+    private static final String ROOMS = "/rooms.json";
+    private static final String SYN = "/synonyms.json";
+
+    private JSONParser jsonParser = new JSONParser();
+    private JSONObject roomsJSON;
+    private JSONObject commandJSON;
+    private JSONArray synJSON;
+    private List<String> commands;
+
+    public static OurJSONParser instantiate() {
+        if (ourParser == null) {
+            try {
+                ourParser = new OurJSONParser();
+            }
+            catch (IOException | ParseException e) {
+                System.out.println(e);
+                System.exit(0);
+            }
+        }
+        return ourParser;
+    }
+    //
+    private OurJSONParser() throws IOException, ParseException {
+        commandJSON = (JSONObject) jsonParser.parse(new InputStreamReader(OurJSONParser.class.getResourceAsStream(COMMANDS)));
+        roomsJSON = (JSONObject) jsonParser.parse(new InputStreamReader(OurJSONParser.class.getResourceAsStream(ROOMS)));
+        synJSON = (JSONArray) jsonParser.parse(new InputStreamReader(OurJSONParser.class.getResourceAsStream(SYN)));
+        System.out.println(synJSON);
+        commands = new ArrayList<>();
+    }
+
+    public List<String> commandParser(String noun, String verb) {
+        commands.clear();
+        JSONArray verbs = (JSONArray) commandJSON.get("verbs");
+        JSONArray nouns = (JSONArray) commandJSON.get("nouns");
+        JSONArray items = (JSONArray) commandJSON.get("items");
+        JSONObject room = (JSONObject) roomsJSON.get(App.currentRoom);
+
+        if (!verbs.contains(verb) && !nouns.contains(noun)) {
+            System.out.println(verb + " " + noun + " is not a valid command");
+        }
+        else {
+            commands.add(verb);
+            commands.add(noun);
+        }
+
+        App.playerActions(commands.get(0), commands.get(1), room, roomsJSON, synJSON, items);
+        return commands;
+    }
+
+//   public List<Location> locationCreator(String file, String noun) {
+//        List<Location> locations = new ArrayList<>();
+//
+//        try {
+////            InputStream in = getFileFromData(file);
+////            InputStreamReader reader = new InputStreamReader(in);
+////            roomsJSON = (JSONArray) jsonParser.parse(reader);
+//            roomsJSON = (JSONArray) jsonParser.parse(new InputStreamReader(OurJSONParser.class.getResourceAsStream(ROOMS)));
+//        }
+//        catch (Exception e) {
+//            System.out.println("Please check location of file: " + file);
+//        }
+//
+//        for (Object o : roomsJSON) {
+//            JSONObject obj = (JSONObject) o;
+//            JSONObject insideRoom = (JSONObject) obj.get(noun);
+//            JSONObject items = (JSONObject) insideRoom.get("item");
+//            Set<String> itemsSet = items.keySet();
+//            ArrayList<String> roomItems = new ArrayList<>(itemsSet);
+//            String desc = (String) insideRoom.get("description");
+//
+//
+//            Location location = new Location (noun, desc, roomItems);
+//
+//            locations.add(location);
+//
+//        }
+//        return locations;
+//   }
+
+}
--- src/com/sprints/controller/App.java
+++ src/com/sprints/controller/App.java
@@ -4,15 +4,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
+import com.sprints.TextParser;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 
 
 public class App {
@@ -20,6 +21,7 @@ public class App {
     public static List<String> inventory = new ArrayList<>();;
     private boolean tutorial = true;
     private boolean gameOver = false;
+    private TextParser parser = new TextParser();
 
     Scanner myObj = new Scanner(System.in);
 
@@ -30,7 +32,6 @@ public class App {
     }
 
     private void start() throws IOException, ParseException {
-
         // possible start tutorial here where player prompted to look, get, and move
         // standard gave play starts after tutorial portion ends
         tutorial = false;
@@ -39,11 +40,9 @@ public class App {
 
             showStatus();
             System.out.printf(">");
-            String playerCommand = myObj.nextLine().toLowerCase(); //changed variable name from "command" to "playerCommand" for better readability
-            List<String> input = new ArrayList<>(Arrays.asList(playerCommand.split(" ")));
+            String playerCommand = myObj.nextLine(); //changed variable name from "command" to "playerCommand" for better readability
 
             //if player inputs "quit" it will break out of the while loop and exit the game----
-            // we can integrate the "start over" logic with this, if the group decides
             if (playerCommand.equals("quit")) {
                 break;
             }
@@ -53,14 +52,11 @@ public class App {
                 inventory.clear();
                 start();
             }
-            parseInput(input);
-
+            parser.playerInput(playerCommand);
         }
     }
 
-
     private void getCommands() {
-        // later use file to read synonyms of the commands
         // help should call this method
         System.out.println("go [direction]\nget [item]\nlook [item]\nhelp (allows you to view in game commands)");
     }
@@ -73,90 +69,22 @@ public class App {
         getCommands();
     }
 
-    private void parseInput(List<String> input) {
-        String noun;
-        String verb;
-        String location = "";
-
-        //if input greater than 2 words
-        if (input.size() > 2) {
-            location = input.get(1) + " " + input.get(2);
-        }
-        if (input.size() >3) {
-            System.out.println("Please enter a valid 2 or 3 word command. Ex: [go north, go west hall, look room]");
-        }
-        else
-        {
-            verb = input.get(0);
-            noun = input.get(1);
-
-            JSONParser parser = new JSONParser();
-            try {
-                //reads from commands.json file
-                FileReader commandReader = new FileReader("data/commands.json"); //
-
-                //reads from synonyms.json
-                FileReader synonymReader = new FileReader("data/synonyms.json");
-
-                JSONObject commandObj = (JSONObject) parser.parse(commandReader);
-
-                //convert the parsed json file into a JSONArray to access values
-                JSONArray synonymObj = (JSONArray) parser.parse(synonymReader);
-
-                //valid items array
-                JSONArray validItems = (JSONArray) commandObj.get("items");
-                //valid verbs array
-                JSONArray validVerbs = (JSONArray) commandObj.get("verbs");
-                //valid nouns array
-                JSONArray validNouns = (JSONArray) commandObj.get("nouns");
-
-                //reads from room.json file
-                FileReader roomReader = new FileReader("data/rooms.json"); //
-                // entire json
-                JSONObject roomsObj = (JSONObject) parser.parse(roomReader);
-                // individual room
-                JSONObject room = (JSONObject) roomsObj.get(currentRoom);
-
-//                JSONObject items = (JSONObject) room.get("item");
-
-                // if input verb is not inside validVerbs array
-                if (synonymObj.contains(verb) || !validVerbs.contains(verb)) {
-                    System.out.println(verb + " is not recognized verb");
-                }
-                // if input noun is not inside validNouns array
-                if (!validNouns.contains(noun)) {
-                    System.out.println(noun + " is not recognized noun");
-                }
-                // pass info playerActions function
-                else {
-                    playerActions(noun, verb, room, roomsObj, location, synonymObj, validItems);
-                }
-
-            } catch (IOException | ParseException e) {
-                System.out.println(e);
-            }
-        }
-
-    }
-
-    private void playerActions(String noun, String verb, JSONObject room, JSONObject roomsObj, String location, JSONArray synonymObj, JSONArray validItems) {
+    public static void playerActions(String verb, String noun, JSONObject room, JSONObject roomsObj, JSONArray synonymObj, JSONArray validItems) {
         JSONArray verbObj1 = (JSONArray) synonymObj.get(0);
         JSONArray verbObj2 = (JSONArray) synonymObj.get(1);
         JSONArray verbObj3 = (JSONArray) synonymObj.get(2);
 
         if (verbObj1.contains(verb)) {
-            locationChange(noun, room, roomsObj, location);
+            locationChange(noun, room, roomsObj);
         }else if (verbObj2.contains(verb)) {
             getItems(noun, room, validItems);
         }else if (verbObj3.contains(verb)) {
             look(noun, room, validItems);
         }
     }
-    private void restart(){
 
-    }
 
-    private void getItems(String noun, JSONObject room, JSONArray validItems) {
+    private static void getItems(String noun, JSONObject room, JSONArray validItems) {
         JSONObject items = (JSONObject) room.get("item");
         if (validItems.contains(noun) && items.containsKey(noun)) {
             System.out.println(noun + " picked up");
@@ -167,9 +95,14 @@ public class App {
         }
     }
 
-    private void locationChange(String noun, JSONObject room, JSONObject roomsObj, String location) {
-        if (roomsObj.containsKey(location)) {
-            currentRoom = location;
+    private static void locationChange(String noun, JSONObject room, JSONObject roomsObj) {
+        if (noun.split(" ").length == 2) {;
+            if (roomsObj.containsKey(noun)) {
+                currentRoom = noun;
+            }
+            else {
+                System.out.println(noun + " is not a known location");
+            }
         }
         else if (room.containsKey(noun)) {
             currentRoom = (String) room.get(noun);
@@ -179,7 +112,7 @@ public class App {
         }
     }
 
-    private void look(String noun, JSONObject room, JSONArray validItems) {
+    private static void look(String noun, JSONObject room, JSONArray validItems) {
         if (noun.equals("here")) {
             System.out.println(room.get("description"));
         }
@@ -212,6 +145,7 @@ public class App {
         if (Files.exists(Path.of("data/text_file/"+ filename))) {
             String file = Files.readString(Path.of("data/text_file/" + filename));
             System.out.println("\u001B[31m" + file + "\u001B[37m");
+
             return file;
         } else {
             throw new IOException("Please verify welcome.txt location");
