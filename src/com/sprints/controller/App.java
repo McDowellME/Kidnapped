a2220c889e3cd962ac1219583559d6d6db3ce14b
Daniel Harris <dan_harris10@yahoo.com>
2022-05-04 12:04:20 -0700

    press enter to continue AB#413

--- src/com/sprints/controller/App.java
+++ src/com/sprints/controller/App.java
@@ -2,6 +2,7 @@ package com.sprints.controller;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.sql.Time;
 import java.text.ParseException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -25,6 +26,8 @@ public class App {
     // hold all methods used to run app
     public void execute() throws IOException, ParseException, InterruptedException {
         welcome();
+        pressEnterToContinue();
+        Console.clear();
         start();
     }
 
@@ -43,12 +46,13 @@ public class App {
             if ("restart".equals(playerCommand)) {
                 restart();
             }
-            if ("help me".equals(playerCommand)) {
+            if ("help".equals(playerCommand)) {
                 getCommands();
             }
             //Clear function coming from external jar
-            Console.clear();
             parser.playerInput(playerCommand);
+            Thread.sleep(1000);
+            Console.clear();
         }
     }
 
@@ -59,27 +63,39 @@ public class App {
     }
 
     // welcomes to game by displaying ascii and break description of game
-    private void welcome() throws IOException {
+    private void welcome() throws IOException, InterruptedException {
         txtFileReader("/title.txt");
+        pressEnterToContinue();
+        Console.clear();
         //read from txt later
-        System.out.println("You awake to find yourself in a twisted escape game.\n Can you gather all the clues and escape with your life in tact before time runs out?");
-        System.out.println("---------------------");
+        String description = "You awake to find yourself in a twisted escape game. Can you gather all the clues and escape\nwith your life in tact before time runs out?";
+        printWithDelays(description);
+        Console.blankLines(2);
+        System.out.println("-----------------------------");
         getCommands();
     }
 
     // restart our game
     void restart() throws IOException, ParseException, InterruptedException {
-        System.out.println("Restarting game...");
-        TimeUnit.SECONDS.sleep(2);
-        Player.getInstance().setCurrentRoom("basement");
-        Player.getInstance().getInventory().clear();
-        start();
+        System.out.println("Are you sure you want to restart?");
+        String q = myObj.nextLine();
+        if ("yes".equals(q) || "y".equals(q)) {
+            System.out.println("Restarting game...");
+            TimeUnit.SECONDS.sleep(2);
+            Player.getInstance().setCurrentRoom("basement");
+            Player.getInstance().getInventory().clear();
+            Console.clear();
+            execute();
+        }
+        else {
+            showStatus();
+        }
     }
 
     void quit() throws IOException, ParseException, InterruptedException {
         System.out.println("Are you sure you want to quit?");
         String q = myObj.nextLine();
-        if ("yes".equals(q)) {
+        if ("yes".equals(q) || "y".equals(q)) {
             System.out.println("quiting the game...");
             TimeUnit.SECONDS.sleep(2);
             gameOver = true;
@@ -91,16 +107,10 @@ public class App {
     }
 
     // used to display status (current room, inventory, room description, etc)
-    private static void showStatus () throws IOException, ParseException {
+    private static void showStatus () throws IOException, ParseException, InterruptedException {
         JSONObject roomObj = OurJSONParser.instantiate().getRoomsJSON();
         JSONObject room = (JSONObject) roomObj.get(Player.getInstance().getCurrentRoom());
         JSONObject items = (JSONObject) room.get("item");
-
-        System.out.println("---------------------------");
-        System.out.println("You are in the " + Player.getInstance().getCurrentRoom());
-        if (Player.getInstance().getCurrentRoom() == "basement") {
-            txtFileReader("/basement.txt");
-        }
 
         if (Player.getInstance().isItemEquipped()) {
             switch (Player.getInstance().getCurrentRoom()){
@@ -114,17 +124,25 @@ public class App {
                 case "kitchen":
                     txtFileReader("/kitchen.txt");
                     break;
+                default:
+                    txtFileReader("/basement.txt");
             }
-            System.out.println(room.get("description"));
-        } else {
-            System.out.println("Too dark to see everything here, you need some light equipped");
-        }
 
-        System.out.println("Inventory:" + Player.getInstance().getInventory());
+            System.out.println("---------------------------");
+            System.out.println("You are in the " + Player.getInstance().getCurrentRoom());
+            Console.blankLines(1);
+            System.out.println(room.get("description"));
+            Console.blankLines(1);
+        } else {
+            System.out.println("Too dark to see everything here, you need some light");
+            Console.blankLines(2);
+        }
         if (room.containsKey("item")) {
             Set<String> roomItems = items.keySet();
-            System.out.println("You see a " + roomItems);
+            System.out.println("You notice: " + roomItems);
+            Console.blankLines(1);
         }
+        System.out.println("Inventory:" + Player.getInstance().getInventory());
         System.out.println(TimeElapsed.getTime());
         System.out.println("-----------------------------");
     }
@@ -143,5 +161,24 @@ public class App {
 
     public boolean isGameOver() {
         return gameOver;
+    }
+
+    private static void printWithDelays(String data)
+            throws InterruptedException {
+        for (char ch:data.toCharArray()) {
+            System.out.print(ch);
+            TimeUnit.MILLISECONDS.sleep(20);
+        }
+    }
+
+    private void pressEnterToContinue() {
+        Console.blankLines(1);
+        System.out.println("Press ENTER to continue");
+        try {
+            System.in.read(new byte[2]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
